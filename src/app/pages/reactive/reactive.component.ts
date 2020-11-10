5c3048373ed79501fd2205243dda7e2995cec614
Jose Luis <jluisvazquez.jv@gmail.com>
2020-11-10 16:18:24 -0600

    199 reactivo validaciones sincronas

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -21,9 +21,9 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario(): void {
     this.forma = this.fb.group({
-      nombre: [''],
-      apellido: [''],
-      correo: [''],
+      nombre  : ['', [Validators.required, Validators.minLength(3)] ],
+      apellido: ['', Validators.required ],
+      correo  : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
     });
   }
 
